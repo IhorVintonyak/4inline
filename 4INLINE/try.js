8b7dd5759da0ced483ed1e39952943ce6541c764
IhorVintonyak <66689738+IhorVintonyak@users.noreply.github.com>
2020-08-09 21:11:36 +0200

    Update try.js
    I have transformed HTML elements in JS list

--- 4INLINE/try.js
+++ 4INLINE/try.js
@@ -270,7 +270,96 @@ function user_button(el){
         circle.classList.remove('cicle')
     }
 
+
+    let element2_1=0
+    let element2_2=0
+    let element2_3=0
+    let element2_4=0
+    let element2_5=0
+    let element2_6=0
+    let element2_7=0
+
+    let element3_1=0
+    let element3_2=0
+    let element3_3=0
+    let element3_4=0
+    let element3_5=0
+    let element3_6=0
+    let element3_7=0
+
+    let element4_1=0
+    let element4_2=0
+    let element4_3=0
+    let element4_4=0
+    let element4_5=0
+    let element4_6=0
+    let element4_7=0
+
+    let element5_1=0
+    let element5_2=0
+    let element5_3=0
+    let element5_4=0
+    let element5_5=0
+    let element5_6=0
+    let element5_7=0
+
+    let element6_1=0
+    let element6_2=0
+    let element6_3=0
+    let element6_4=0
+    let element6_5=0
+    let element6_6=0
+    let element6_7=0
+
+
+    element1_1=document.getElementById("1_line_1coin").getAttribute("value")
+        if (element1_1==="red"){
+            element1_1=1
+        }
+    element1_2=document.getElementById("1_line_2coin").getAttribute("value")
+        if (element1_2==="red"){
+            element1_2=1
+        }
+    element1_3=document.getElementById("1_line_3coin").getAttribute("value")
+        if (element1_3==="red"){
+            element1_3=1
+        }
+    element1_4=document.getElementById("1_line_4coin").getAttribute("value")
+        if (element1_4==="red"){
+            element1_4=1
+        }
+    element1_5=document.getElementById("1_line_5coin").getAttribute("value")
+        if (element1_5==="red"){
+            element1_5=1
+        }
+    element1_6=document.getElementById("1_line_6coin").getAttribute("value")
+        if (element1_6==="red"){
+            element1_6=1
+        }
+    element1_7=document.getElementById("1_line_7coin").getAttribute("value")
+        if (element1_7==="red"){
+            element1_7=1
+        }
+
+
+
+    line1=[element1_1,element1_2,element1_3,element1_4,element1_5,element1_6,element1_7]
+    line2=[element2_1,element2_2,element2_3,element2_4,element2_5,element2_6,element2_7]
+    line3=[element3_1,element3_2,element3_3,element3_4,element3_5,element3_6,element3_7]
+    line4=[element4_1,element4_2,element4_3,element4_4,element4_5,element4_6,element4_7]
+    line5=[element5_1,element5_2,element5_3,element5_4,element5_5,element5_6,element5_7]
+    line6=[element6_1,element6_2,element6_3,element6_4,element6_5,element6_6,element6_7]
+    console.log(line1)
+    console.log(line2)
+    console.log(line3)
+    console.log(line4)
+    console.log(line5)
+    console.log(line6)
+
+
 }
+
+
 /*
 function control_table(){
     let list=[2]
